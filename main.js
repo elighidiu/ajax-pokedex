3a526b0ff7cf97f170d95876603ccbfc28a970c0
Your Nameeli <elighidiu@yahoo.com>
2021-05-07 10:02:29 +0200

    display the data for previous species

--- main.js
+++ main.js
@@ -2,30 +2,50 @@
   
     async function dataPoke(poke){
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`);
-        const responseSpecies = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${poke}`);           
+        const responseSpecies = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${poke}`); 
+                 
         const data = await response.json();
         const dataSpecies = await responseSpecies.json();
         
-        console.log(data.name);
-        console.log(data.id);
-        console.log(data.sprites.front_default);
+        if(dataSpecies.evolves_from_species!==null) {
+            let previousPoke = dataSpecies.evolves_from_species;  //get the name of the previous species
+            const responsePrevious = await fetch(`https://pokeapi.co/api/v2/pokemon/${previousPoke.name}`);  //using the name of the previous species get more data (like the id and image - this info is in our first used api)
+
+            const dataPrevious = await responsePrevious.json();
+            document.getElementById("prevName").innerHTML=`Evolved from ${dataPrevious.name}`;
+            document.getElementById("prevId").innerHTML=dataPrevious.id;
+
+            let prevImageArea = document.getElementById("prevImage"); 
+            const prevImg = document.createElement("img"); //create img
+            prevImg.src = dataPrevious.sprites.front_default;
+            prevImageArea.appendChild(prevImg); //append the image to the div 
+
+        } else {
+            console.log("No previous evolution");
+        }
 
         document.getElementById("name").innerHTML=data.name;
         document.getElementById("id").innerHTML=data.id;
               
         let imageArea = document.getElementById("image"); 
-        //create img and append it to the body
-        const img = document.createElement("img");
+        const img = document.createElement("img"); //create img
         img.src = data.sprites.front_default;
-        imageArea.appendChild(img);
+        imageArea.appendChild(img); //append the image to the div 
 
-        // get the list with the moves
-       let ulArea = document.getElementById("movesList");
-        data.moves.forEach(element => { 
+        // get a list with first 5 moves
+        let ulArea = document.getElementById("movesList");
+        for (let i=0; i<5; i++){
             const listElement = document.createElement("li");
-            listElement.innerHTML = element.move.name;
-            ulArea.appendChild(listElement);             
-        });
+            listElement.innerHTML = data.moves[i].move.name;
+            ulArea.appendChild(listElement);  
+        }
+
+          /*    Instead of displaying all moves, I will only display 5
+                data.moves.forEach(element => { 
+                const listElement = document.createElement("li");
+                listElement.innerHTML = element.move.name;
+                ulArea.appendChild(listElement);             
+          }); */
 
       };
     
@@ -35,6 +55,9 @@
       let text;
       function getInput(e){
             text = e.target.value;
+            if(isNaN("text")){ //if the input is not a number, then change the string to lower case
+                text = text.toLowerCase();
+            }
          // console.log(text);
             return text;
       };
@@ -46,9 +69,13 @@
       });
 
       function removeData(){
-        var oldImg=document.querySelector("#image").lastChild;
+        var oldImg = document.querySelector("#image").lastChild;
         document.querySelector("#image").removeChild(oldImg);
+        var oldPrevImg = document.querySelector("#prevImage").lastChild;
+        document.querySelector("#prevImage").removeChild(oldPrevImg);
         document.querySelector("#movesList").innerHTML="";
+        document.querySelector("#prevName").innerHTML="";
+        document.querySelector("#prevId").innerHTML="";
         }
 
 })();
